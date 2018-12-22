899cc018fc93ef831bca4a70457a8718d4f9810d
jschrom311 <jschrom311@yahoo.com>
2018-12-21 23:16:03 -0600

    first commit

---
+++ fibfizz.js
@@ -0,0 +1,26 @@
+//Fizz Buzz 
+
+/***
+ * Write a program that prints the numbers from 1 to 100.
+ * But for multiples of three print 'Fizz' instead of the number and
+ * for the multiples of five print 'Buzz'.  For numbers which are
+ * multiples of both three and five print 'FizzBuzz'.
+ * 
+ */
+
+
+
+ /***
+  * A series of numbers in which each number (Fibonacci number)
+  * is the usm of the two preceding numbers.  The simplest is the series
+  * 1, 1, 2, 3, 5, 8, etc.
+  * 
+  * Recursive solution:
+  *     function fib(n){
+  *         if(n<2){
+  *             return n;
+  * }
+  *     return fib(n-1) + fib(n-2);
+  * }
+  * 
+  */
