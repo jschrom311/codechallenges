f7fcdf563e0cd56051407053dcc9f48774bff70b
jschrom311 <jschrom311@yahoo.com>
2018-12-24 16:54:36 -0600

    reversestring

---
+++ reverseString.js
@@ -0,0 +1,30 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+//Solution one done with prototypes
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
+
+module.exports = reverse;
+
+
+function reverse(str) {
+    let reversed = '';
+    for (let character of str) {
+        reversed = character + reversed;
+    }
+    return reversed;
+}
+
+module.exports = reverse;
+
+function reverse(str) {
+    return str.split('').reduce((rev, char) => char + rev, '');
+}
+
+module.exports = reverse;
