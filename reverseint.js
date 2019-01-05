42f009e3cb34423eb0bf76fa4396826aff7b8477
jschrom311 <jschrom311@yahoo.com>
2019-01-04 18:41:27 -0600

    working on reverseint

---
+++ reverseint.js
@@ -0,0 +1,19 @@
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(n) {
+    parseInt(n.toString().split('').reverse().join(''));
+    if (Math.sign(n) = 1)
+    return n;
+    else {
+        return
+    }
+}
+
+module.exports = reverseInt;
