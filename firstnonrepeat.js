b0b51133e8266cce73ab016e64fa5e5109d9421b
jschrom311 <jschrom311@yahoo.com>
2019-01-07 02:45:35 -0600

    nonrepeating

---
+++ firstnonrepeat.js
@@ -0,0 +1,7 @@
+/***
+ * Given a string s consisting of small English letters, find and return
+ * the first instance of a non-repeating character in it. If there is no
+ * such character, return '_'.
+ */
+
+ function firstNonRepeatingCharacter(s){}
