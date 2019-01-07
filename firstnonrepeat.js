2d01b32b07ae32e1ab0f0e0c629c0bbc8c28537e
jschrom311 <jschrom311@yahoo.com>
2019-01-07 02:58:47 -0600

    nonrepeat soln

--- firstnonrepeat.js
+++ firstnonrepeat.js
@@ -4,4 +4,12 @@
  * such character, return '_'.
  */
 
- function firstNonRepeatingCharacter(s){}
+function firstNonRepeatingCharacter(s) {
+    for (i = 0; i < s.length; i++) {
+        let c = s.charAt(i);
+        if (s.indexOf(c) == i && s.indexOf(c, i + 1) == -1) {
+            return c;
+        }
+    }
+    return '_';
+}
