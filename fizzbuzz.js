822714dfd15670956b7ad1db2bce88c149ac8f59
jschrom311 <jschrom311@yahoo.com>
2019-04-18 14:50:56 -0500

    fizzbuzz solution

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -9,7 +9,20 @@ multiples of both three and five print 'fizzbuzz'.
 
 function fizzBuzz(n) {
     for (let i = 1; i<=n; i++) {
-        
+        //multiple of 3 & 5 (should only be multiples of 15)
+        //could do (i % 15 === 0)
+        if (i % 3 === 0 && i % 5 == 0) {
+            console.log('fizzbuzz');
+        }
+        else if (i % 3 === 0) {
+            console.log('fizz');
+        }
+        else if (i % 5 === 0) {
+            console.log('buzz');
+        }
+        else {
+            console.log(i);
+        }
     }
 }
 
