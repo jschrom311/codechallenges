9c877cf758176bfe6e5e9ba320554621f63a5cc1
jschrom311 <jschrom311@yahoo.com>
2019-04-18 14:40:46 -0500

    fizzbuzz challenge

---
+++ fizzbuzz.js
@@ -0,0 +1,14 @@
+/* Directions
+Write a program that console logs the numbers from 1 to n.
+For multiples of three print 'fizz' instead of the number
+and for multiples of five print 'buzz'.  For numbers which are
+multiples of both three and five print 'fizzbuzz'.
+*/
+//The real challenge of this is determining when the number is
+//a multiple of both three and five - modulo operator helps this!
+
+function fizzBuzz(n) {
+
+}
+
+module.exports = fizzBuzz;
