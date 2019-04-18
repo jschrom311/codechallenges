2b95c301c94358aff9b62d9c1bf0e92e8f088c0b
jschrom311 <jschrom311@yahoo.com>
2019-04-18 13:35:55 -0500

    maxchar challenge

---
+++ maxchar.js
@@ -0,0 +1,14 @@
+/* Directions
+Given a string, return the character taht is most commonly used in the string.
+Examples:
+maxChar("abcccccd") === "c"
+maxChar ("apple 1231111") === "1"
+*/
+//General logic for solving: convert string into object with characters as keys
+//and counts as values.  Then go through object and return greatest key value.
+
+function maxChar(str) {
+
+}
+
+module.exports = maxChar;
