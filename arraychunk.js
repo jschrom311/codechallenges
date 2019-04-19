d997949c36e8c776f82d861f7eaabddac3ddd07b
jschrom311 <jschrom311@yahoo.com>
2019-04-19 12:21:44 -0500

    arraychunk solution

--- arraychunk.js
+++ arraychunk.js
@@ -6,8 +6,22 @@ chunk ([1, 2, 3, 4], 2) --> [[1, 2], [3, 4]]
 chunk ([1, 2, 3, 4, 5], 2) --> [[1, 2], [3, 4], [5]]
 */
 
+//Solution 1
 function chunk(array, size) {
+    const chunked = [];
+    
+    for (let element of array) {
+        const last = chunked[chunked.length - 1];
 
+        if (!last || last.length === size) {
+            chunked.push([element]);
+        }
+        else {
+            last.push(element);
+        }
+    }
+
+    return chunked;
 }
 
 module.exports = chunk;
