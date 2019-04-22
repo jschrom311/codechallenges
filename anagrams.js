834643a929877f1449524c4631b656ab3019548c
jschrom311 <jschrom311@yahoo.com>
2019-04-22 17:46:01 -0500

    anagram solution1

--- anagrams.js
+++ anagrams.js
@@ -12,9 +12,23 @@ word.replace(/[^\w]/g, "").toLowerCase();
 */
 
 function anagrams (stringA, stringB) {
+    const aCharMap = buildCharMap(stringA);
+    const bCharMap = buildCharMap(stringB);
     
+    //Make sure str same length using object keys
+    if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length){
+        return false;
+    }
+    for (let char in aCharMap) {
+        if (aCharMap[char] !== bCharMap[char]) {
+            return false;
+        }
+    }
+    
+    return true;
 }
 
+//helper function
 function buildCharMap(str) {
     const charMap = {};
     for (let char of str.replace(/[^\w]/g, '').toLowerCase()){
