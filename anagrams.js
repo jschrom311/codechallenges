654639fac4e8e0f9af35f2a06f55efc66c06b539
jschrom311 <jschrom311@yahoo.com>
2019-04-22 17:22:39 -0500

    anagram setup

---
+++ anagrams.js
@@ -0,0 +1,12 @@
+/* Directions
+Check to see if two provided strings are anagrams of each other.
+One string is an anagram of another if it uses the same characters
+in the same quantity.  Only consider characters, not spaces or
+punctuation.  Consider capital letters the same as lower case.
+*/
+
+function anagrams (stringA, stringB) {
+
+}
+
+module.exports = anagrams;
