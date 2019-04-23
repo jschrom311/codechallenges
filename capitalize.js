86012b7239f52a27cbd2ed5ef897f2fa52d3d036
jschrom311 <jschrom311@yahoo.com>
2019-04-23 14:34:27 -0500

    capitalize setup

---
+++ capitalize.js
@@ -0,0 +1,10 @@
+/*Directions
+Write a function that accepts a string.  The function should capitalize the first letter
+of each word in the string then return the capitalized string.
+*/
+
+function capitalize(str) {
+
+}
+
+module.exports = capitalize;
