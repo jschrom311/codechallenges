78cf5d842b9711e787dab9161802c23bfb66c449
jschrom311 <jschrom311@yahoo.com>
2019-04-23 14:46:05 -0500

    capitalize soln 1

--- capitalize.js
+++ capitalize.js
@@ -7,7 +7,11 @@ of each word in the string then return the capitalized string.
 //The other will use methods from js library(slice, toUpperCase, join)
 
 function capitalize(str) {
-
+    const words = [];
+    for (let word of str.split(' ')) {
+        words.push(word[0].toUpperCase() + word.slice(1));
+    }
+    return words.join(' ');
 }
 
 module.exports = capitalize;
