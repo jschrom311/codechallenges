72782c310e64917d0dc11cf70052a3e4a4fa0736
jschrom311 <jschrom311@yahoo.com>
2019-04-29 12:53:41 -0500

    pyramid iterative soln

--- pyramid.js
+++ pyramid.js
@@ -18,12 +18,19 @@ pyramid(3)
 
 //Iterative solution
 function pyramid(n) {
+    const midpoint = Math.floor((2 * n - 1) / 2);
     for (let row = 0; row < n; row++) {
         let level = '';
 
         for (let column = 0; column < 2 * n - 1; column++) {
-
+            if (midpoint - row <= column && midpoint + row >= column) {
+                level += '#';
+            }
+            else {
+                level += ' ';
+            }
         }
+        console.log(level);
     }
 }
 
