482e9f9c0330e5c9520aead17a24298696d33cd3
jschrom311 <jschrom311@yahoo.com>
2019-05-01 09:44:35 -0500

    vowels iterative soln

--- vowels.js
+++ vowels.js
@@ -3,8 +3,18 @@ Write a function that returns the number of vowels
 in a string (A, E, I, O, U).
 */
 
+//Iterative soln
 function vowels(str) {
+    let counter = 0;
+    const vowelcheck = ['a', 'e', 'i', 'o', 'u'];
 
+    for (let char of str.toLowerCase()) {
+        //includes method simplifies code, could use comparative statements (if char === 'a' || char === 'e')
+        if (vowelcheck.includes(char)) {
+            counter++;
+        }
+    }
+    return counter;
 }
 
 module.exports = vowels;
