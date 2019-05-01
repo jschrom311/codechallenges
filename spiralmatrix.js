9bf28d87b71ca57a508d5cdb9c143b11ee5d3712
jschrom311 <jschrom311@yahoo.com>
2019-05-01 12:34:15 -0500

    spiral matrix soln

--- spiralmatrix.js
+++ spiralmatrix.js
@@ -11,12 +11,46 @@ matrix(3)
 [7, 6, 5]]
 */
 
-function matrix(n, start_row = 0, end_row = 0) {
+function matrix(n) {
     const results = [];
 
     for (let i = 0; i < n; i++) {
         results.push([]);
     }
+
+    let counter = 1;
+    let startColumn = 0;
+    let endColumn = n - 1;
+    let startRow = 0;
+    let endRow = n - 1;
+
+    while (startColumn <= endColumn && startRow <= endRow) {
+        for (let i = startColumn; i <= endColumn; i++) {
+            results[startRow][i] = counter;
+            counter++;
+        }
+        startRow++;
+
+        for (let i = startRow; i <= endRow; i++) {
+            results[i][endColumn] = counter;
+            counter++;
+        }
+        endColumn--;
+
+        for (let i = endColumn; i >= startColumn; i--) {
+            result[endRow][i] = counter;
+            counter++;
+        }
+        endRow--;
+
+        for (let i = endRow; i >= startRow; i--) {
+            results[i][startColumn] = counter;
+            counter++;
+        }
+        startColumn++;
+    }
+    return results;
+
 }
 
 module.exports = matrix;
