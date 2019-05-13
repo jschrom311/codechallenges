0ef4d53780c05afe0cf4e8b4aa1afb3c7b8a3a79
jschrom311 <jschrom311@yahoo.com>
2019-05-13 13:09:46 -0500

    fib complete

--- fib.js
+++ fib.js
@@ -35,9 +35,27 @@ module.exports = fib;
 
 //recursive soln with better efficiency(runtime complexity) using memoization
 
-function fib(n) {
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+        return result;
+    };
 
 }
+function fib(n) {
+    if (n < 2) {
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+fib = memoize(fib);
+
 module.exports = fib;
 
 
