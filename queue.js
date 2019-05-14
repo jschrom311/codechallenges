64101a602105ca5284ba6e6effa210b9f8a448a1
jschrom311 <jschrom311@yahoo.com>
2019-05-14 18:32:02 -0500

    queue setup

---
+++ queue.js
@@ -0,0 +1,9 @@
+/*Directions
+Create a queue data structure.  The queue should be a class
+with methods 'add' and 'remove'.  Adding to the queue should
+store an element until it is removed.
+*/
+
+class Queue{}
+
+module.exports = Queue;
