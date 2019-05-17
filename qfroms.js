edfd1b4563617ca11b1575c348def7f552cd0914
jschrom311 <jschrom311@yahoo.com>
2019-05-16 21:35:56 -0500

    qfroms soln

--- qfroms.js
+++ qfroms.js
@@ -8,6 +8,43 @@
  const Stack = require('./stack');
 
  class Queue {
+     constructor () {
+         this.first = new Stack();
+         this.second = new Stack();
+     }
+
+     add(data) {
+        this.first.push(data);
+     }
+
+     remove() {
+        while (this.first.peek()) {
+            this.second.push(this.first.pop());
+        }
+
+        const data = this.second.pop();
+
+        while (this.second.peek()) {
+            this.first.push(this.second.pop());
+        }
+
+        return data;
+
+     }
+
+     peek() {
+         while (this.first.peek()) {
+             this.second.push(this.first.pop());
+         }
+         const data = this.second.peek();
+
+         while (this.second.peek()) {
+             this.first.push(this.second.pop());
+         }
+
+         return data;
+         
+     }
 
  }
 
