743e529114fd6e1e8b3a2c7550ce16e0f523cf3a
jschrom311 <jschrom311@yahoo.com>
2019-05-16 21:18:10 -0500

    qfroms setup

---
+++ qfroms.js
@@ -0,0 +1,14 @@
+/**
+ * Directions
+ * Implement a Queue datastructure using two stacks.
+ * Do not create an array inside of the Queue class.  Queue
+ * should implement the methods add, remove and peek.
+ */
+
+ const Stack = require('./stack');
+
+ class Queue {
+
+ }
+
+ module.exports = Queue;
