0beec10856f8c9dac9848a11eab0ca30e5bfccb0
jschrom311 <jschrom311@yahoo.com>
2019-05-16 21:04:21 -0500

    stack setup

---
+++ stack.js
@@ -0,0 +1,11 @@
+/**Directions
+ * Create a stack data structure. The stack should be a class with
+ * methods push, pop and peek.  Adding an element to the stack should
+ * store it until it's removed.
+ */
+
+ class Stack {
+
+ }
+
+ module.exports = Stack;
