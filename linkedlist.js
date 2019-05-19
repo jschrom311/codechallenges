3c4c0ad1bce63ca9d02b9ee1a1e692a66b49cedc
jschrom311 <jschrom311@yahoo.com>
2019-05-19 17:56:02 -0500

    linkedlist setup

---
+++ linkedlist.js
@@ -0,0 +1,16 @@
+/**
+ * Directions --
+ * Implement classes Node and Linked Lists.
+ * Node should have two properties, 'data' and 'next'.
+ * 
+ */
+
+ class Node {
+
+ }
+
+ class LinkedList {
+
+ }
+
+ module.exports = {Node, LinkedList};
