67db418680cf7c57d6e1a443c4938a8130af6b71
jschrom311 <jschrom311@yahoo.com>
2019-05-29 11:27:22 -0500

    fromlast setup

---
+++ fromlast.js
@@ -0,0 +1,11 @@
+/**
+ * Given a linked list and integer n, return the element n spaces from the last node
+ * in the list.  Do not call the 'size' method of the linked list.  Assume that n
+ * will always be less than the length of the list.
+ */
+
+ function fromLast(list, n) {
+
+ }
+
+ module.exports = fromLast;
