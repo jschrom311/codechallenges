6e9a000f7ef9bcc4536fb66d0ea3706fa84738cd
jschrom311 <jschrom311@yahoo.com>
2019-05-28 20:41:54 -0500

    midpoint setup

---
+++ midpoint.js
@@ -0,0 +1,11 @@
+/**
+ * Return the 'middle' node of a linked list.
+ * If the list has an even number of elements,
+ * return the node at the end of the first half of the list.
+ * Don't use a counter variable, don't retrieve the size
+ * of the list, and only iterate through the list once.
+ */
+
+ function midpoint(list) {
+     
+ }
