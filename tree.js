81a0f356203b919cc6e498d3a1a5ec2995729845
jschrom311 <jschrom311@yahoo.com>
2019-05-31 19:19:10 -0500

    tree setup

---
+++ tree.js
@@ -0,0 +1,15 @@
+/**
+ * Create a node class.  The constructor should accept an argument that gets
+ * assigned to the data property and initialize an empty array for storing
+ * children.  The node class should have 'add' and 'remove' methods.
+ * 
+ * Create a tree class.  The tree constructor should initialize a 'root'
+ * property to null.
+ * 
+ * Implement breadth first search traversal and depth first search traversal
+ * on the tree class.
+ */
+
+ class Node {}
+
+ class Tree {}
