0ff8a8b4e12c3891f031f38784ec66db0477af1b
jschrom311 <jschrom311@yahoo.com>
2019-06-05 13:50:31 -0500

    bst setup

---
+++ bst.js
@@ -0,0 +1,15 @@
+/**
+ * Directions:
+ * Implement a Node class to create a binary search tree.
+ * The constructor should initialize values data, left, and right.
+ * 
+ * Implement the insert method for the Node class. Insert should accept
+ * an argument data, then create and insert a new node at the appropriate
+ * location in the tree.
+ */
+
+ class Node {
+
+ }
+
+ module.exports = Node;
