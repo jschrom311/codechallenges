4253d992a7fb1f509d0e330d49276a85ebcfc017
jschrom311 <jschrom311@yahoo.com>
2019-06-04 20:12:41 -0500

    levelwidth setup

---
+++ levelwidth.js
@@ -0,0 +1,12 @@
+/**
+ * Given the root node of a tree, return an array
+ * where each element is the width of the tree
+ * at each level.
+ * 
+ */
+
+ function levelWidth(root) {
+
+ }
+
+ module.exports = levelWidth;
