fb80a0bce22cc0e10151a8b340ee76679eec3333
jschrom311 <jschrom311@yahoo.com>
2019-06-12 14:43:13 -0500

    validate bst

--- validate.js
+++ validate.js
@@ -5,8 +5,13 @@
  * the parent.
  */
 
- function validate(node) {
-
+ function validate(node, min = null, max = null) {
+    if (max !== null && node.data > max) {
+        return false;
+    }
+    if (min !== null && node.data < min) {
+        return false;
+    }
  }
 
  module.exports = validate;
