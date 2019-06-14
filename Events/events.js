06a9e18219bc6525f2e3ba5cd9c24ce70ba1caf4
jschrom311 <jschrom311@yahoo.com>
2019-06-14 18:46:35 -0500

    events soln

--- Events/events.js
+++ Events/events.js
@@ -5,16 +5,30 @@
  */
 
  class Events {
+     constructor() {
+         this.events = {};
+     }
      //register event handler
      on (eventName, callback){
-
+        if (this.events[eventName]) {
+            this.events[eventName].push(callback);
+        }
+        else {
+            this.events[eventName] = [callback];
+        }
      }
      //trigger all callbacks associated with given eventName
      trigger (eventName) {
-
+        if (this.events[eventName]) {
+            for (let cb of this.events[eventName]) {
+                cb();
+            }
+        }
      }
      //remove all event handlers associated with given eventName
      off (eventName) {
-
+        delete this.events[eventName];
      }
  }
+
+ module.exports = Events;
