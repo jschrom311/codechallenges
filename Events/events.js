4e3a6bc3f441ae1f6d5b5ff7a3eb7b2a431c9745
jschrom311 <jschrom311@yahoo.com>
2019-06-14 18:21:59 -0500

    events setup

---
+++ Events/events.js
@@ -0,0 +1,20 @@
+/**
+ * Create an eventing library out of the Events class.
+ * The Events class should have methods 'on', 'trigger', and 'off'.
+ * 
+ */
+
+ class Events {
+     //register event handler
+     on (eventName, callback){
+
+     }
+     //trigger all callbacks associated with given eventName
+     trigger (eventName) {
+
+     }
+     //remove all event handlers associated with given eventName
+     off (eventName) {
+
+     }
+ }
