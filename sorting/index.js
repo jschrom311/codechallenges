30513bb3813ec4e9ee59e438560a6d3bad5ca8a8
jschrom311 <jschrom311@yahoo.com>
2019-06-16 18:10:00 -0500

    bubbleSort soln

--- sorting/index.js
+++ sorting/index.js
@@ -3,7 +3,19 @@
  */
 
  function bubbleSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < (arr.length - i - 1); j++) {
+            if (arr[j] > arr[j + 1]) {
+                const lesser = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = lesser;
+            }
+            else {
+                return;
+            }
+        }
+    }
+    return arr;
  }
 
  function selectionSort(arr) {
@@ -11,5 +23,5 @@
  }
 
  function mergeSort(arr) {
-     
+
  }
