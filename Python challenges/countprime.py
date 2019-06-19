fe074b8d89bfa0a6079f6e07b1136c3421b0dd8d
jschrom311 <jschrom311@yahoo.com>
2019-06-19 11:01:37 -0500

    countprimespy

---
+++ Python challenges/countprime.py
@@ -0,0 +1,10 @@
+#Write a function that returns the number of prime
+#numbers that exist up to and including a given number.
+#
+#By convention treat 0 and 1 as not prime.
+#
+#Ex.
+#count_primes(100) --> 25
+#
+
+def count_primes(num):
