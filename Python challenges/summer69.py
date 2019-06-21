07153e21b12535e28a853ed11ecfcb1fcd21fafd
jschrom311 <jschrom311@yahoo.com>
2019-06-21 11:50:19 -0500

    summer69py soln

--- Python challenges/summer69.py
+++ Python challenges/summer69.py
@@ -10,4 +10,19 @@
 #
 
 def summer_69(arr):
-    pass
+    total = 0
+    add = True
+    for num in arr:
+        while add:
+            if num != 6:
+                total += num
+                break
+            else:
+                add = False
+        while not add:
+            if num != 9:
+                break
+            else:
+                add = True
+                break
+    return total
