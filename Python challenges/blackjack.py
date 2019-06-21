08e46e90ac38f82fd5934be1544c83a98b2f8e45
jschrom311 <jschrom311@yahoo.com>
2019-06-21 11:42:39 -0500

    blackjackpy soln

--- Python challenges/blackjack.py
+++ Python challenges/blackjack.py
@@ -10,3 +10,9 @@
 #
 
 def blackjack(a, b, c):
+    if sum([a,b,c]) <= 21:
+        return sum([a,b,c])
+    elif 11 in [a,b,c] and sum([a,b,c]) - 10 <= 21:
+        return sum([a,b,c]) - 10
+    else:
+        return 'BUST'
