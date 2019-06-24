01b6c52c842c353f33b1914db0f6f708b204035c
jschrom311 <jschrom311@yahoo.com>
2019-06-24 18:38:40 -0500

    guessgamepy setup

---
+++ Python challenges/guessgame.py
@@ -0,0 +1,12 @@
+#Guessing Game-
+#Write a program that picks a random integer from 1 to 100
+#and has players guess the number. Rules:
+#1. If a player's guess is less than 1 or greater than 100 return 'OUT OF BOUNDS'
+#2. On a player's first turn if their guess is:
+#(a) within 10 of the number, return 'WARM'
+#(b)further than 10 from the number, return 'COLD'
+#3. On all subsequent turns, if a guess is:
+#(a) closer to the number than the previous guess, return 'WARMER'
+#(b) farther from the number than the previous guess, return 'COLDER'
+#4.When the player's guess equals the number, return correct and the number of guesses.
+
