19a837727a1794d64c4c2e6e6adef2216c2f0907
jschrom311 <jschrom311@yahoo.com>
2019-06-26 13:38:57 -0500

    findduppy soln

--- Python challenges/finddup.py
+++ Python challenges/finddup.py
@@ -3,3 +3,14 @@
 #Ex. find_dup([1,3,3]) --> True
 #find_dup([1,3,1,3]) --> False
 #
+
+def find_dup(nums):
+    for i in range(0, len(nums)-1):
+        if nums[i] == 3 and nums[i+1] == 3:
+            return True
+    return False
+
+
+#another option inside the for loop:
+#if nums[i:i+2] == [3,3]:
+#   return True
