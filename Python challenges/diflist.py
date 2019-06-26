9148460e1bdbcdbed5271de53c5df8fb183d6017
jschrom311 <jschrom311@yahoo.com>
2019-06-26 14:38:25 -0500

    diflistpy setup

---
+++ Python challenges/diflist.py
@@ -0,0 +1,9 @@
+#Implement a difference function which subtracts
+#one list from another and returns the result.
+#It should remove all values from list a, which are
+#present in list b.
+#
+#Ex. array_diff([1,2],[1]) --> [2]
+#array_diff([1,2,2,2,3],[2]) --> [1,3]
+#
+
