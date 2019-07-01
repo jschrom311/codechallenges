7e4669610bc9cc9c83a04df2799ec9d622bf28a7
jschrom311 <jschrom311@yahoo.com>
2019-07-01 09:06:11 -0500

    squareitpy setup

---
+++ Python challenges/squareit.py
@@ -0,0 +1,7 @@
+#Square every digit of a number.
+#For example, if 9119 is run through the function, 811181 will come out.
+#The function accepts an integer and returns an integer.
+#
+
+def square_digits(num):
+    pass
