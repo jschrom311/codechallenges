dc044cd8d41898e2cd32b0fed6785582191c65a6
jschrom311 <jschrom311@yahoo.com>
2019-07-01 09:39:13 -0500

    squareitpy alternate soln

--- Python challenges/squareit.py
+++ Python challenges/squareit.py
@@ -4,4 +4,10 @@
 #
 
 def square_digits(num):
-    return int(''.join((str(int(n)**2) for n in str(int(num)))))
+    return int(''.join(str(int(n)**2) for n in str((num)))
+
+def squared_digits(num):
+    d_squared = ""
+    for d in str(num):
+        d_squared += str(int(d)**2)
+    return int(d_squared)
