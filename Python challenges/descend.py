c2eab71aeb60b42ee1f9f063c3258a65cbc56d1e
jschrom311 <jschrom311@yahoo.com>
2019-07-09 20:11:43 -0500

    descendpy setup

---
+++ Python challenges/descend.py
@@ -0,0 +1,10 @@
+#Your task is to make a function that can take any non-negative integer as 
+# a argument and return it with its digits in descending order. 
+# Essentially, rearrange the digits to create the highest possible number.
+#
+#Ex.
+#input: 21544 => output: 54421
+#input: 12385469 => 98654321
+
+def descending_order(num):
+    pass
