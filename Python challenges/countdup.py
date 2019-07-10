b63c00214defd7ed28e94754851d40477fdd63dc
jschrom311 <jschrom311@yahoo.com>
2019-07-10 14:58:45 -0500

    countduppy soln

--- Python challenges/countdup.py
+++ Python challenges/countdup.py
@@ -1,4 +1,13 @@
 '''Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.'''
+from collections import Counter
 
 def duplicate_count(text):
-    # Your code goes here
+    #counter variable
+    duplicates = 0
+    #iterate over character list turned into dictionary
+    for k, v in Counter(list(text.lower())).items():
+        #increment the duplicate counter
+        if v > 1:
+            duplicates += 1
+
+    return duplicates
